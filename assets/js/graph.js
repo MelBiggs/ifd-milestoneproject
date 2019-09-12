ca2c8d03566d21727bc9907a2ab2f9c5d06c4735
MelBiggs <melissabiggs16@gmail.com>
2019-09-12 17:22:11 +0000

    Added mode pie, filter and fixed row chart

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -13,8 +13,8 @@ function makeGraphs(error, spotifyData) {
     showDanceability(ndx);
     //show_energy_to_danceability_correlation(ndx);
     showKey(ndx);
-    //showSongFilter(ndx);
-
+    showSongFilter(ndx);
+    showMode(ndx);
 }
 
 
@@ -72,7 +72,7 @@ function averageSongLength(ndx) {
 function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
     var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds + " mins";
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
 function topArtists(ndx) {
@@ -88,187 +88,193 @@ function topArtists(ndx) {
         .data(function(group) { return group.top(18); });
 
     chart.render();
+}
 
-    //Line Chart - Energy
+//Line Chart - Energy
 
-    function showEnergy(ndx) {
-        var chart = dc.lineChart("#energy");
-        var dim = ndx.dimension(function(data) {
-            return parseFloat(data.energy).toFixed(1);
-        });
+function showEnergy(ndx) {
+    var chart = dc.lineChart("#energy");
+    var dim = ndx.dimension(function(data) {
+        return parseFloat(data.energy).toFixed(1);
+    });
 
-        var group = dim.group();
+    var group = dim.group();
 
-        console.log(group);
+    console.log(group);
 
-        chart
-            .width(500)
+    chart
+        .width(500)
             .height(300)
-            // .y(d3.scale.linear().domain([0,1]))
-            .x(d3.scale.linear().domain([0.30, 1.00]))
-            // .clipPadding(50)
-            .brushOn(false)
-            .yAxisLabel("Song Count")
-            .xAxisLabel("Energy")
-            .margins({ top: 10, right: 50, bottom: 75, left: 75 })
-            .dimension(dim)
-            .group(group)
-            .on('renderlet', function(chart) {
-                chart.selectAll('rect').on('click', function(d) {
-                    console.log('click!', d);
-                });
+        // .y(d3.scale.linear().domain([0,1]))
+        .x(d3.scale.linear().domain([0.30, 1.00]))
+        // .clipPadding(50)
+        .brushOn(false)
+        .yAxisLabel("Song Count")
+        .xAxisLabel("Energy")
+        .margins({ top: 10, right: 50, bottom: 75, left: 75 })
+        .dimension(dim)
+        .group(group)
+        .elasticY(true)
+        .on('renderlet', function(chart) {
+            chart.selectAll('rect').on('click', function(d) {
+                console.log('click!', d);
             });
-        chart.render();
-    }
-
-    // Line Chart - Danceability
-
-    function showDanceability(ndx) {
-        var chart = dc.lineChart("#danceability");
-
-        var dim = ndx.dimension(function(data) {
-            return parseFloat(data.danceability).toFixed(1);
         });
 
-        var group = dim.group();
+    chart.render();
+}
 
-        console.log(group);
+// Line Chart - Danceability
 
-        chart.width(400)
-            .height(300)
-            .x(d3.scale.linear().domain([0.30, 1.00]))
-            .brushOn(false)
-            .yAxisLabel("Frequency")
-            .xAxisLabel("Danceability")
-            .margins({ top: 10, right: 2, bottom: 75, left: 10 })
-            .dimension(dim)
-            .group(group)
-            .on('renderlet', function(chart) {
-                chart.selectAll('rect').on('click', function(d) {
-                    console.log('click!', d);
-                });
+function showDanceability(ndx) {
+    var chart = dc.lineChart("#danceability");
+
+    var dim = ndx.dimension(function(data) {
+        return parseFloat(data.danceability).toFixed(1);
+    });
+
+    var group = dim.group();
+
+    console.log(group);
+
+    chart.width(400)
+        .height(300)
+        .x(d3.scale.linear().domain([0.30, 1.00]))
+        .brushOn(false)
+        .yAxisLabel("Frequency")
+        .xAxisLabel("Danceability")
+        .margins({ top: 10, right: 2, bottom: 75, left: 10 })
+        .dimension(dim)
+        .group(group)
+        .elasticY(true)
+        .on('renderlet', function(chart) {
+            chart.selectAll('rect').on('click', function(d) {
+                console.log('click!', d);
             });
-
-        chart.render();
-    }
-
-    // // Scatter plot
-
-    // function show_energy_to_danceability_correlation(ndx) {
-    //     var energyDim = ndx.dimension(dc.pluck("energy"));
-    //     var danceDim = ndx.dimension(function(d) {
-    //         return [d.energy, d.danceability];
-    //     });
-    //     var danceGroup = danceDim.group();
-    //     var minEnergy = energyDim.bottom(1)[0].energy;
-    //     var maxEnergy = energyDim.top(1)[0].energy;
-
-    //     dc.scatterPlot("#energy_to_danceability")
-    //         .width(500)
-    //         .height(350)
-    //         .x(d3.scale.linear().domain([minEnergy, maxEnergy]))
-    //         .brushOn(true)
-    //         .symbolSize(6)
-    //         .clipPadding(10)
-    //         .yAxisLabel("Danceability")
-    //         // .title(function(d) {
-    //         //     return "Danceability"
-    //         .dimension(danceDim)
-    //         .group(danceGroup);
-    //     .margins({ top: 10, right: 50, bottom: 75, left: 75 });
-
-    //}
-
-    //Filter for Songs
-
-    // function showSongFilter(ndx) {
-    //     let dim = dataFor2017.dimension(dc.pluck("name"));
-    //     let group = dim.group();
-
-    //     dc.selectMenu("#name-selection")
-    //         .dimension(dim)
-    //         .group(group)
-    //         .promptText("All Songs")
-    //         .multiple(false)
-    //         .title(function(d) {
-    //             return d.key;
-    //         });
-    // }
-
-    //Pie Chart - Key
-
-    function showKey(ndx) {
-        var chart = dc.pieChart("#key");
-
-        // Transform the numbers into musical keys in letters
-        var keyDimension = ndx.dimension(function(data) {
-            return transformKey(parseInt(data.key));
         });
 
-        var group = keyDimension.group();
+    chart.render();
+}
 
-        // Create the chart
-        chart
-            .width(600)
-            .height(400)
-            .innerRadius(100)
-            .dimension(keyDimension)
-            .group(group)
-            .legend(dc.legend())
-            .on('pretransition', function(chart) {
-                chart.selectAll('text.pie-slice').text(function(d) {
-                    return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2 * Math.PI) * 100) + '%';
-                });
+// // Scatter plot
+
+// function show_energy_to_danceability_correlation(ndx) {
+//     var energyDim = ndx.dimension(dc.pluck("energy"));
+//     var danceDim = ndx.dimension(function(d) {
+//         return [d.energy, d.danceability];
+//     });
+//     var danceGroup = danceDim.group();
+//     var minEnergy = energyDim.bottom(1)[0].energy;
+//     var maxEnergy = energyDim.top(1)[0].energy;
+
+//     dc.scatterPlot("#energy_to_danceability")
+//         .width(500)
+//         .height(350)
+//         .x(d3.scale.linear().domain([minEnergy, maxEnergy]))
+//         .brushOn(true)
+//         .symbolSize(6)
+//         .clipPadding(10)
+//         .yAxisLabel("Danceability")
+//         // .title(function(d) {
+//         //     return "Danceability"
+//         .dimension(danceDim)
+//         .group(danceGroup);
+//     .margins({ top: 10, right: 50, bottom: 75, left: 75 });
+
+//}
+
+//Filter for Songs
+
+function showSongFilter(ndx) {
+    let dim = ndx.dimension(dc.pluck("name"));
+    let group = dim.group();
+
+    var selectionMenu = dc.selectMenu("#name-selection");
+
+    selectionMenu
+        .dimension(dim)
+        .group(group)
+        .promptText("All Songs")
+        .multiple(false)
+        .title(function(d) {
+            return d.key;
+        })
+        .render();
+}
+
+//Pie Chart - Key
+
+function showKey(ndx) {
+    var chart = dc.pieChart("#key");
+
+    // Transform the numbers into musical keys in letters
+    var keyDimension = ndx.dimension(function(data) {
+        return transformKey(parseInt(data.key));
+    });
+
+    var group = keyDimension.group();
+
+    // Create the chart
+    chart
+        .width(600)
+        .height(400)
+        .innerRadius(100)
+        .dimension(keyDimension)
+        .group(group)
+        .legend(dc.legend())
+        .on('pretransition', function(chart) {
+            chart.selectAll('text.pie-slice').text(function(d) {
+                return d.data.key + ' ' + Math.round(dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2 * Math.PI) * 100)) + '%';
             });
+        });
 
-        chart.render();
+    chart.render();
+}
+
+// function to transfor the number scale for music key to letters
+function transformKey(key) {
+    var keyString;
+
+    switch (key) {
+        case 0:
+            keyString = "C";
+            break;
+        case 1:
+            keyString = "C♯/D♭";
+            break;
+        case 2:
+            keyString = "D";
+            break;
+        case 3:
+            keyString = "D♯/E♭";
+            break;
+        case 4:
+            keyString = "E";
+            break;
+        case 5:
+            keyString = "F";
+            break;
+        case 6:
+            keyString = "F♯/G♭";
+            break;
+        case 7:
+            keyString = "G";
+            break;
+        case 8:
+            keyString = "G♯/A♭";
+            break;
+        case 9:
+            keyString = "A";
+            break;
+        case 10:
+            keyString = "A♯/B♭";
+            break;
+        case 11:
+            keyString = "B";
+            break;
     }
 
-    // function to transfor the number scale for music key to letters
-    function transformKey(key) {
-        var keyString;
-
-        switch (key) {
-            case 0:
-                keyString = "C";
-                break;
-            case 1:
-                keyString = "C♯/D♭";
-                break;
-            case 2:
-                keyString = "D";
-                break;
-            case 3:
-                keyString = "D♯/E♭";
-                break;
-            case 4:
-                keyString = "E";
-                break;
-            case 5:
-                keyString = "F";
-                break;
-            case 6:
-                keyString = "F♯/G♭";
-                break;
-            case 7:
-                keyString = "G";
-                break;
-            case 8:
-                keyString = "G♯/A♭";
-                break;
-            case 9:
-                keyString = "A";
-                break;
-            case 10:
-                keyString = "A♯/B♭";
-                break;
-            case 11:
-                keyString = "B";
-                break;
-        }
-
-        return keyString;
-    }
+    return keyString;
 }
 
 
@@ -289,3 +295,46 @@ function topArtists(ndx) {
 //         .xAxisLabel("Valence")
 //         .yAxis().ticks(5);
 // }
+
+function showMode(ndx) {
+    var chart = dc.pieChart("#mode");
+
+    // Transform the numbers into mode in letters
+    var modeDimension = ndx.dimension(function(data) {
+        return transformMode(parseInt(data.mode));
+    });
+
+    var group = modeDimension.group();
+
+    // Create the chart
+    chart
+        .width(600)
+        .height(400)
+        .innerRadius(100)
+        .dimension(modeDimension)
+        .group(group)
+        .legend(dc.legend())
+        .on('pretransition', function(chart) {
+            chart.selectAll('text.pie-slice').text(function(d) {
+                return d.data.key + ' ' + Math.round(dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2 * Math.PI) * 100)) + '%';
+            });
+        });
+
+    chart.render();
+}
+
+// function to transform the number scale for mode to minor and major
+function transformMode(mode) {
+    var modeString;
+
+    switch (mode) {
+        case 0:
+            modeString = "Minor";
+            break;
+        case 1:
+            modeString = "Major";
+            break;
+    }
+    return modeString;
+
+}
